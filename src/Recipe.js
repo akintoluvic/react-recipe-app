959569f1669cf4cbf83ad7e8db2ff4ac45199e88
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-07-17 11:17:58 +0100

    changes to 3 columns layout

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,14 +4,16 @@ import style from "./recipe.module.css";
 const Recipe = ({title, calories, image, ingredients}) => {
     return ( 
         <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
-            <img src={image} alt="" />
+            <img src={image} className={style.image} alt="" />
+            <div className={style.body}>
+                <h1>{title}</h1>
+                <ol>
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ol>
+                <p>{calories}</p>
+            </div>
         </div>
      );
 }
