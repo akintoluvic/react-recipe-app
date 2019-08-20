32e81bbfa660bc7f2d29add530868e799fe3e627
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-08-20 12:32:28 +0100

    init

--- src/Header.js
+++ src/Header.js
@@ -16,7 +16,7 @@ function Header() {
     }
     const brandNAme = {
         marginLeft: '10px',
-        fontFamily: 'Work Sans'
+        // fontFamily: 'Work Sans'
     }
     return (
         <>
