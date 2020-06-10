5a422238b7d0c847b007b0aae7b07d93a875039d
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-10 19:41:42 +0100

    fix push new changes

--- src/Recipes.js
+++ src/Recipes.js
@@ -7,7 +7,7 @@ const Recipes = ({recipes}) => {
         <div class="container px-5 mx-auto">
           <div class="flex flex-col">
             <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-              <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
+              <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Space The Meal Frontier</h1>
             </div>
           </div>
           <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
